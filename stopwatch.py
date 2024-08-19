45854da09fbe0e194975098b11a0b6f3fb0014e6
bryanmelendez <bryanmelendez2323@gmail.com>
2024-08-18 23:43:21 -0700

    stopwatch

---
+++ stopwatch.py
@@ -0,0 +1,11 @@
+class Stopwatch:
+    def __init__(self):
+        self.start_time = 0.0
+        self.end_time = None
+        self.elapsed_time = 0.0
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
