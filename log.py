3b634fa144bcd80fb7f8003e7529c0ff4fc1c5e3
bryanmelendez <bryanmelendez2323@gmail.com>
2024-08-27 15:00:18 -0700

    made log into a class

--- log.py
+++ log.py
@@ -2,46 +2,59 @@ import json
 import os
 from stopwatch import Stopwatch
 
-log_file = "study_log.json"
-backup_file = "backup_study_log.json"
-backup = None
-session = 0
 
-# Driver code
-sw = Stopwatch()
+class Log:
+    def __init__(self):
+        self.log_file = "study_log.json"
+        self.backup_file = "backup_study_log.json"
+        self.backup = None
+        self.session = 0
 
-sw.start()
-sw.print_stats()
+    def start_session(self):
+        sw = Stopwatch()
 
-if os.path.exists(log_file):
-    os.rename(log_file, backup_file)
-    with open(backup_file, "r") as infile:
-        backup = json.load(infile)
+        sw.start()
+        sw.print_stats()
 
-    session = backup[0]["session"]
-    print("Session: ", session)
+        if os.path.exists(self.log_file):
+            os.rename(self.log_file, self.backup_file)
+            with open(self.backup_file, "r") as infile:
+                self.backup = json.load(infile)
 
-# creates new list to be written to file
-log = list()
+            # get the previous session number
+            self.session = self.backup[0]["session"]
+            print("Session: ", self.session)
 
-log_entry = {
-        "session": session+1,
-        "date": sw.date_string,
-        "start time": sw.format_start_time,
-        "end time": sw.format_end_time,
-        "elapsed time": sw.format_elapsed_time,
-        "description": "",
-}
+        # creates new list to be written to file
+        log = list()
 
-log.append(log_entry)
-# append old list to new list
-if backup:
-    log = log + backup
-print(log)
+        log_entry = {
+                "session": self.session+1,
+                "date": sw.date_string,
+                "start time": sw.format_start_time,
+                "end time": sw.format_end_time,
+                "elapsed time": sw.format_elapsed_time,
+                "description": "",
+        }
 
-json_object = json.dumps(log, indent=4)
+        log.append(log_entry)
+        # append old list to new list
+        if self.backup:
+            log = log + self.backup
+        print(log)
 
-with open(log_file, "a") as outfile:
-    outfile.write(json_object)
+        json_object = json.dumps(log, indent=4)
 
-os.system("nvim study_log.json")
+        with open(self.log_file, "a") as outfile:
+            outfile.write(json_object)
+
+        os.system("nvim study_log.json")
+
+
+def main():
+    log = Log()
+    log.start_session()
+
+
+if __name__ == "__main__":
+    main()
