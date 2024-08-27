9938eb1b701945253c7425daa38e2d923d98cd57
bryanmelendez <bryanmelendez2323@gmail.com>
2024-08-26 20:09:37 -0700

    started logs

---
+++ log.py
@@ -0,0 +1,33 @@
+import json
+import os
+from stopwatch import Stopwatch
+
+log_file = "study_log.json"
+backup_file = "backup_study_log.json"
+backup = None
+
+# Driver code
+sw = Stopwatch()
+
+sw.start()
+sw.print_stats()
+
+log = {
+        # convert these to strings
+        "start time": sw.format_start_time,
+        "end time": sw.format_end_time,
+        "elapsed time": sw.format_elapsed_time
+}
+
+if os.path.exists(log_file):
+    os.rename(log_file, backup_file)
+    with open(backup_file, "r") as infile:
+        backup = infile.read()
+
+json_object = json.dumps(log, indent=4)
+
+with open(log_file, "a") as outfile:
+    outfile.write(json_object)
+    if backup:
+        outfile.write("\n")
+        outfile.write(backup)
--- stopwatch.py
+++ stopwatch.py
@@ -8,9 +8,9 @@ class Stopwatch:
         self.start_time = 0.0
         self.end_time = None
         self.elapsed_time = 0.0
-        self.output_elapsed_time = None
-        self.output_start_time = None
-        self.output_end_time = None
+        self.format_elapsed_time = None
+        self.format_start_time = None
+        self.format_end_time = None
 
     def start(self):
         # Run stopwatch until user enters ctrl-c
@@ -28,19 +28,13 @@ class Stopwatch:
         self.calc_stats()
 
     def calc_stats(self):
-        self.output_start_time = datetime.datetime.fromtimestamp(int(self.start_time)).time()
-        self.output_end_time = datetime.datetime.fromtimestamp(int(self.end_time)).time()
-        self.output_elapsed_time = datetime.timedelta(seconds=int(self.elapsed_time))
+        self.format_start_time = datetime.datetime.fromtimestamp(int(self.start_time)).time()
+        self.format_end_time = datetime.datetime.fromtimestamp(int(self.end_time)).time()
+        self.format_elapsed_time = datetime.timedelta(seconds=int(self.elapsed_time))
 
+    # move this to other module
     def print_stats(self):
-        print("\nSession started at: ", self.output_start_time)
-        print("Session ended at: ", self.output_end_time)
-        print("Session length: ", self.output_elapsed_time)
+        print("\nSession started at: ", self.format_start_time)
+        print("Session ended at: ", self.format_end_time)
+        print("Session length: ", self.format_elapsed_time)
         print()
-
-
-# Driver code
-stopwatch = Stopwatch()
-
-stopwatch.start()
-stopwatch.print_stats()
