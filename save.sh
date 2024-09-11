8ca81b3e4151117336dbacbd7745fad0f0f08b1c
bryanmelendez <bryanmelendez2323@gmail.com>
2024-09-10 17:17:30 -0700

    save script

--- save.sh
+++ save.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-cp -v study_log.yaml $notes/1-personal/study-log/
+SAVE_PATH="$notes/1-personal/study-log/"
+
+if [ -f "$SAVE_PATH/study_log.md" ]; then
+    echo "Creating a backup"
+    cp $SAVE_PATH/study_log.md $SAVE_PATH/backup.md
+fi
+echo "Saving file to $SAVE_PATH"
+cp -v study_log.yaml $SAVE_PATH
 mv -v $notes/1-personal/study-log/study_log.yaml $notes/1-personal/study-log/study_log.md
